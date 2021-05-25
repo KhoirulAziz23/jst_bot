6f93a799892fa1da69430006e86509ba881791c7
KhoirulAziz23 <84756729+KhoirulAziz23@users.noreply.github.com>
2021-05-26 02:56:06 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,23 +10,51 @@ const token = '1546740995:AAGDFa9qum9GZj-xJFKiNShD283TMka3b3s'
 const bot = new TelegramBot(token, {polling: true});
 
 
-// bots
+// Main Menu bots
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+// input requires I and V
+state = 0;
+bot.onText(/\/predict/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        'Masukan Nilai i|v , contoh 7|8'
+    );
+    state = 1;
 });
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), //string to float
+                parseFloat(s[1])
+            ]
+       ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                'Nilai V yang Di Prediksi adalah $(jres[0]) Volt'
+            );
+            
+            bot.sendMessage(
+                msg.chat.id,
+                'Nilai P yang Di Prediksi adalah $(jres[1]) Watt'
+            );
+        })
+    }else{
+        state = 0
+    }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
